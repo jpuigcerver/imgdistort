e3f0b21e56c138952a200a0c57d22cd5a1d61c5d
jpuigcerver <joapuipe@gmail.com>
2018-02-20 04:29:13 +0100

    Updated gitignore and added build.sh script for travis

---
+++ travis/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e;
+
+mkdir build;
+cd build;
+cmake -DWITH_CUDA=OFF -DWITH_GLOG=OFF -DWITH_IPP=OFF -DWITH_PYTORCH=ON \
+      -DCMAKE_BUILD_TYPE=DEBUG ..;
+make VERBOSE=1;
+
+cd pytorch;
+python setup.py bdist_wheel;
+pip install $(find dist/ -name "*.whl");
